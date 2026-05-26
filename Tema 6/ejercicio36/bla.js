07e6c2f5808004e887a25c96fce3290b0d35683a
Jesus-Sanchez-dela-Riva <sanchezdelariva.jesus@jgvaldemora.org>
2026-05-26 13:16:56 +0200

    Actividad 36 terminada

--- Tema 6/ejercicio36/bla.js
+++ Tema 6/ejercicio36/bla.js
@@ -1,15 +1,43 @@
-class moto{
+class Moto{
     constructor(a,m,p){
         this.marca=a
         this.modelo=m
         this.peso=p
-        console.log(`Se ha creado una moto de marca ${this.marca} el modelo ${this.modelo} y un peso en vacio ${this.peso}kg`)
     }
     echarGasolina(gasolina){
         this.peso+=gasolina
-        console.log(`Al echar ${gasolina}L de gasolina el peso ha subido a ${this.peso}kg`)
+        actualizarDom()
+    }
+
+    mover(){
+        const limite = window.innerWidth - 300
+        if(posicion >= limite){
+            posicion = 50
+        }else{
+            posicion += 100
+        }
+
+        moto.style.left=posicion +"px"
     }
 }
 
-const ktm=new moto("Ktm", "Duke", 150)
-ktm.echarGasolina(15)
+const ktm=new Moto("Ktm", "Duke", 150)
+const info=document.getElementById("info")
+const moto=document.getElementById("moto")
+let posicion = 50
+
+function actualizarDom(){
+    info.innerHTML=`Moto: ${ktm.marca} ${ktm.modelo} | Peso: ${ktm.peso}kg`
+}
+
+actualizarDom()
+
+document.addEventListener("keydown", (e)=>{
+    if(e.key.toLowerCase() == "g"){
+        ktm.echarGasolina(10)
+    }
+
+    if(e.key.toLowerCase() == "m"){
+        ktm.mover()
+    }
+})
